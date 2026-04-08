a88b6ed57e908610d4d007e98fdd739edea25298
AnoopRahul <anooprahul2006@gmail.com>
2026-04-08 11:55:07 +0530

    Update DocumentWorkflow.java

--- src/main/java/com/workflow/DocumentWorkflow.java
+++ src/main/java/com/workflow/DocumentWorkflow.java
@@ -51,5 +51,8 @@ public class DocumentWorkflow {
 
         workflow.approve();
         System.out.println("After Approval: " + workflow.getCurrentState());
+         while (true) {
+        Thread.sleep(10000);
+    }
     }
 }
